37d569e3140cef986a9640197edcf5ab899fb66a
Shivansh kumar <151950082+Shivansh3452@users.noreply.github.com>
2025-10-04 12:13:35 +0530

    Time: 3 ms (40.16%) | Memory: 10.6 MB (50.01%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        stack<string> st;
+        string word="";
+        string ans="";
+        for(char& c:s){
+            if(c==' '){
+                if(!word.empty()){
+                    st.push(word);
+                    word="";
+                } 
+            }
+                else{
+                    word+=c;
+                }
+        }
+        // Last wale word ke liye
+        if(!word.empty()){
+            st.push(word);
+        }
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+            if(!st.empty()){
+                ans+=" ";
+            }
+        }
+        return ans;
+    }
+};
