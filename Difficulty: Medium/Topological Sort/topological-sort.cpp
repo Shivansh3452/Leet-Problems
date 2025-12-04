6afbff9ca5b725ef80868dcd68fc203352c56fde
Shivansh kumar <151950082+Shivansh3452@users.noreply.github.com>
2025-12-04 11:38:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological Sort/topological-sort.cpp
+++ Difficulty: Medium/Topological Sort/topological-sort.cpp
@@ -1,37 +1,37 @@
+
+//              KAHN ALHORITHM
+
+
 class Solution {
   public:
-  
-    void DFS(vector<vector<int>> &adj,int u,vector<bool> &visited,stack<int> &s){
-        visited[u]=true;
-        for(auto& v:adj[u]){
-            if(!visited[v]){
-                DFS(adj,v,visited,s);
-            }
-        }
-        s.push(u);
-    }
-  
-    vector<int> topoSort(int V, vector<vector<int>>& edges) {
-        vector<vector<int>> adj(V);
-        for(auto& e: edges){
-            int u=e[0];
-            int v=e[1];
-            
+    vector<int> topoSort(int N, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(N);
+        vector<int> indegree(N,0);
+        for(auto& edge: edges){
+            int u=edge[0];
+            int v=edge[1];
             adj[u].push_back(v);
+            indegree[v]++;
         }
-        vector<bool> visited(V,false);
-        vector<int> res;
-        stack<int> s;
-        for(int i=0;i<V;i++){
-            if(!visited[i]){
-                DFS(adj,i,visited,s);
+        queue<int> q;
+        for(int i=0;i<N;i++){
+            if(indegree[i]==0){
+                q.push(i);
             }
         }
-        while(!s.empty()){
-            res.push_back(s.top());
-            s.pop();
+        vector<int> result;
+        while(!q.empty()){
+            int u=q.front();
+            q.pop();
+            result.push_back(u);
+            
+            for(auto& v:adj[u]){
+                indegree[v]--;
+                if(indegree[v]==0){
+                    q.push(v);
+                }
+            }
         }
-        return res; 
-        
+        return result;
     }
 };
