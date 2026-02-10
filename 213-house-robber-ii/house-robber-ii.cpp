6fe009e765d929568fedb9d0f0f1bf2f617f3ab4
Shivansh kumar <151950082+Shivansh3452@users.noreply.github.com>
2026-02-11 00:48:22 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (96.88%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,24 +1,34 @@
 class Solution {
 public:
-    int memo[101];
-    int helper(int i, int n, vector<int>& nums) {
-        if (i > n)
-            return 0;
-        if (memo[i] != -1)
-            return memo[i];
-        int steal = nums[i] + helper(i + 2, n, nums);
-        int skip = helper(i + 1, n, nums);
-        return memo[i] = max(steal, skip);
-    }
-
+    int t[10000];
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 1)
+        int n=nums.size();
+        if(n==1)
             return nums[0];
-        memset(memo, -1, sizeof(memo));
-        int take_0th = helper(0, n - 2, nums);
-        memset(memo, -1, sizeof(memo));
-        int take_1th = helper(1, n - 1, nums);
-        return max(take_0th, take_1th);
+        if (n == 2) 
+            return max(nums[0], nums[1]);
+
+        //case 1 when we take first house and thus not take the last house
+        t[0]=0;
+        t[1]=nums[0];
+        for(int i=2;i<=n-1;i++){
+            int skip=t[i-1];
+            int take=nums[i-1]+t[i-2];
+            t[i]=max(skip,take);
+        }
+        int result1=t[n-1];
+
+
+        // case 2 when we do not take the first house
+        t[0]=0;
+        t[1]=0;
+        for(int i=2;i<=n;i++){
+            int skip=t[i-1];
+            int take=nums[i-1]+t[i-2];
+            t[i]=max(skip,take);
+        }
+        int result2=t[n];
+
+        return max(result1,result2);
     }
 };
