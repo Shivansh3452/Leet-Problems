9614843642f4238a264c0fc1e33542e732f975d6
Shivansh kumar <151950082+Shivansh3452@users.noreply.github.com>
2026-02-12 11:17:00 +0530

    Time: 75 ms (50.49%) | Memory: 14.4 MB (62.62%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-    int t[2501][2501];
-    int helper(int n,vector<int> &nums,int i,int prev){
-        if(i>=n)
-            return 0;
-        if(prev!=-1 && t[i][prev]!=-1)
-            return t[i][prev];
-        int take=0;
-        if(prev==-1||nums[i]>nums[prev]){
-            take=1+helper(n,nums,i+1,i);
-        }
-        int skip=helper(n,nums,i+1,prev);
-
-        if(prev!=-1)
-            t[i][prev]=max(take,skip);
-        return max(take,skip);
-    }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        memset(t,-1,sizeof(t));
-        return helper(n,nums,0,-1);
+        vector<int> t(n,1);
+        int maxLIS=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i]){
+                    t[i]=max(t[i],t[j]+1);
+                    maxLIS=max(maxLIS,t[i]);
+                }
+            }
+        }
+        return maxLIS;
     }
 };
