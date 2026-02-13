f46c72ef24e51a701fd692c9e53dacd6c02c5196
Shivansh kumar <151950082+Shivansh3452@users.noreply.github.com>
2026-02-13 17:12:30 +0530

    Time: 135 ms (75.95%) | Memory: 105.7 MB (67.23%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
-    long long maxAlternatingSum(vector<int>& nums) {
 
-        // 0 → even state (+)
-        // 1 → odd state (-)
-        int n=nums.size();
-        vector<vector<long>>t(n+1,vector<long>(2,0));     //even ke liye 0 and odd ke liye 1
-        for(int i=1;i<=n;i++){
-            //even ke liye
-            t[i][0]=max(t[i-1][0],t[i-1][1]+nums[i-1]);
-            //odd ke liye
-             t[i][1]=max(t[i-1][1],t[i-1][0]-nums[i-1]);
+    long long t[100001][2];
+    long long helper(int i,int n,bool flag,vector<int>& nums){
+        if(i==nums.size())
+            return 0;
+        if(t[i][flag]!=-1)
+            return t[i][flag];
+        long long skip=helper(i+1,n,flag,nums);
+        long long take;
+        if(flag){
+            take=nums[i]+helper(i+1,n,!flag,nums);
         }
-        return t[n][0];
+        else{
+            take=-nums[i]+helper(i + 1,n,!flag,nums);
+        }
+        return t[i][flag]=max(take,skip);
+    }
+    long long maxAlternatingSum(vector<int>& nums) {
+        int n=nums.size();
+        bool flag=true;
+        memset(t,-1,sizeof(t));
+        return helper(0,n,flag,nums);
     }
 };
