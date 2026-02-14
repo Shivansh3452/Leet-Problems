d25eb352f71d800fcdb4e881c42b8528bf049567
Shivansh kumar <151950082+Shivansh3452@users.noreply.github.com>
2026-02-14 15:45:28 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (49.73%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n<=0)
+        if(n==-2147483648)
             return false;
-        return (n&n-1)==0;
+        return __builtin_popcount(n)==1;
     }
 };
