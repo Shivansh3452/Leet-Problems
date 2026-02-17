e5da587c48b10c9880170318f591e6ff99db37fd
Shivansh kumar <151950082+Shivansh3452@users.noreply.github.com>
2026-02-17 18:36:41 +0530

    Time: 35 ms (25.87%) | Memory: 11.3 MB (41.34%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    int t[1001][1001];
-    bool isPalindrome(int i,int j,string& s){
-        if(i>j)
-            return true;
-        if(t[i][j]!=-1)
-            return t[i][j];
-        if(s[i]==s[j])
-            return t[i][j]=isPalindrome(i+1,j-1,s);
-        return t[i][j]=false;
-    }
     int countSubstrings(string s) {
         int n=s.size();
+        vector<vector<bool>> t(n,vector<bool>(n,false));
         int count=0;
-        memset(t,-1,sizeof(t));
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(isPalindrome(i,j,s))
+//state=t[i][j]=true:s[i:j] is a palindrome substring where i and j are inclusive
+        for(int L=1;L<=n;L++){
+            for(int i=0;i+L<=n;i++){
+                int j=i+L-1;
+                if(L==1)    //size is 1;
+                    t[i][j]=true;
+                else if(L==2)  //size is 2
+                    t[i][j]=(s[i]==s[j]);
+                else 
+                    t[i][j]=(s[i]==s[j]&&t[i+1][j-1]);
+                if(t[i][j])
                     count++;
-            }
+                }
         }
         return count;
     }
