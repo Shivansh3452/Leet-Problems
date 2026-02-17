006d6dfbd17ed66223ee55a9f310c52e2790c24f
Shivansh kumar <151950082+Shivansh3452@users.noreply.github.com>
2026-02-17 17:28:08 +0530

    Time: 30 ms (16.62%) | Memory: 10.5 MB (90.51%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     int t[501][501];
-    int helper(int m,int n,string& s1,string& s2){
-        if(m==0||n==0)
-            return m+n;
-        if(t[m][n]!=-1)
-            return t[m][n];
-        if(s1[m-1]==s2[n-1])
-            return helper(m-1,n-1,s1,s2);
-        else{
-            int inset=1+helper(m,n-1,s1,s2);
-            int delet=1+helper(m-1,n,s1,s2);
-            int leplace=1+helper(m-1,n-1,s1,s2);
-            return t[m][n]=min({inset,delet,leplace});
-        }
-        return -1;
-    }
     int minDistance(string s1, string s2) {
         int m=s1.size();
         int n=s2.size();
-        memset(t,-1,sizeof(t));
-        return helper(m,n,s1,s2);
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                if(i==0||j==0)
+                    t[i][j]=i+j;
+                else if(s1[i-1]==s2[j-1])
+                    t[i][j]=t[i-1][j-1];
+                else
+                    t[i][j]=1+min({t[i-1][j],t[i][j-1],t[i-1][j-1]});
+            }
+        }
+        return t[m][n];
     }
 };
