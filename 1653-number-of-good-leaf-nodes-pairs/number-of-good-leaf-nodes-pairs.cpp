cd0c3046d2df856818770f9b1124e0e7ac2dd4fd
Shivansh kumar <151950082+Shivansh3452@users.noreply.github.com>
2026-03-23 23:49:31 +0530

    Time: 25 ms (58.33%) | Memory: 35 MB (48.77%) - LeetSync

--- 1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.cpp
+++ 1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.cpp
@@ -12,49 +12,34 @@
 class Solution {
 public:
 
-    void makeGraph(TreeNode* root,TreeNode* prev,unordered_map<TreeNode*,vector<TreeNode*>>& adj,unordered_set<TreeNode*>& st){
+    vector<int> helper(TreeNode* root,int& distance,int& count){
         if(!root)
-            return;
+            return {0};
         if(!root->left&&!root->right)
-            st.insert(root);        //leaf node
-        if(prev){
-            adj[root].push_back(prev);
-            adj[prev].push_back(root);
-        }
-        makeGraph(root->left,root,adj,st);
-        makeGraph(root->right,root,adj,st);
-    }
-    int countPairs(TreeNode* root, int distance) {
-        unordered_map<TreeNode*,vector<TreeNode*>>adj;
-        unordered_set<TreeNode*>st;
-
-        makeGraph(root,nullptr,adj,st); 
-
-        int count=0;    //count good nodes pair
-        for(auto& leaf:st){
-            //BFS hit and see if you can find another leaf nodes within distance
-            queue<TreeNode*> q;
-            unordered_set<TreeNode*> visited;
-            q.push(leaf);
-            visited.insert(leaf);
-
-            for(int level=0;level<=distance;level++){//dist se >wale ko kyohi loge
-                int n=q.size();
-                while(n--){
-                    TreeNode* curr=q.front();
-                    q.pop();
-                    if(curr!=leaf&&st.count(curr)){
-                        count++;
-                    }
-                    for(auto& ngbr:adj[curr]){
-                        if(!visited.count(ngbr)){
-                            q.push(ngbr);
-                            visited.insert(ngbr);
-                        }
-                    }
-                }
+            return {1};
+        vector<int> left_d=helper(root->left,distance,count);
+        vector<int> right_d=helper(root->right,distance,count);
+        
+        for(auto& l:left_d){
+            for(auto& r:right_d){
+                if(l!=0&&r!=0&&l+r<=distance)
+                    count++;
             }
         }
-        return count/2;
+        vector<int> curr;
+        for(auto& l:left_d){
+            if(l!=0&&l+1<=distance)
+                curr.push_back(l+1);
+        }
+        for(auto& r:right_d){
+            if(r!=0&&r+1<=distance)
+                curr.push_back(r+1);
+        }
+        return curr;
+    }
+    int countPairs(TreeNode* root, int distance) {
+        int count=0;
+        helper(root,distance,count);
+        return count;
     }
 };
