fcf32c44d65c84618e9c06d1e22bbbf96905bb6c
Shivansh kumar <151950082+Shivansh3452@users.noreply.github.com>
2026-04-01 18:07:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -1,35 +1,32 @@
 class Solution {
   public:
   
-    bool isCycle(vector<vector<int>> &adj,int u,vector<bool> &visited,vector<bool> &currRev){
-        visited[u]=true;
-        currRev[u]=true;
-        for(int &v:adj[u]){
-            if(!visited[v] && isCycle(adj,v,visited,currRev)){
-                return true;
-            }
-            else if(currRev[v]==true){
-                return true;
-            }
+    vector<vector<int>> buildgraph(int V,vector<vector<int>> &edges){
+        vector<vector<int>> adj(V);
+        for(auto& e: edges){
+            adj[e[0]].push_back(e[1]);
         }
-        currRev[u]=false;
+        return adj;
+    }
+    bool isCycle(vector<vector<int>>& adj,int u,vector<bool>& vis,vector<bool>& inRec){
+        vis[u]=true;
+        inRec[u]=true;
+        for(auto& v:adj[u]){
+            if(!vis[v]&&isCycle(adj,v,vis,inRec))
+                return true;
+            else if(inRec[v]==true)
+                return true;
+        }
+        inRec[u]=false;
         return false;
     }
-  
     bool isCyclic(int V, vector<vector<int>> &edges) {
-        vector<vector<int>> adj(V);
-        for(auto &e: edges){
-            int u=e[0];
-            int v=e[1];
-            adj[u].push_back(v);
-        }
-        vector<bool> visited(V,false);
-        vector<bool> currRev(V,false);
-        
+        vector<vector<int>> adj=buildgraph(V,edges);
+        vector<bool>vis(V);
+        vector<bool>inRec(V);
         for(int i=0;i<V;i++){
-            if(!visited[i]&& isCycle(adj,i,visited,currRev)){
+            if(!vis[i]&&isCycle(adj,i,vis,inRec))
                 return true;
-            }
         }
         return false;
     }
