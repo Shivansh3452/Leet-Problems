cfe4c01e0906b6e34ed04ba3d0e9c8d1c9287243
Shivansh kumar <151950082+Shivansh3452@users.noreply.github.com>
2026-04-02 17:50:15 +0530

    Time: 20 ms (7.45%) | Memory: 23.1 MB (5.08%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,33 +1,41 @@
 class Solution {
 public:
-    unordered_map<int,vector<int>> buildmap(vector<vector<int>>& prereq){
+
+    unordered_map<int,vector<int>> buildgraph(int n,vector<vector<int>> prereq,vector<int>& indeg){
         unordered_map<int,vector<int>> mpp;
-        for(auto& it:prereq){
-            mpp[it[1]].push_back(it[0]);
+        for(auto& e:prereq){
+            mpp[e[1]].push_back(e[0]);
+            indeg[e[0]]++;
         }
         return mpp;
     }
-    bool isCycle(int i,vector<bool>& rec,vector<bool>& vis,unordered_map<int,vector<int>>& mpp){
-        vis[i]=true;
-        rec[i]=true;
-        for(auto& v:mpp[i]){
-            if(!vis[v]&&isCycle(v,rec,vis,mpp))
-                return true;
-            else if(rec[v])
-                return true;
-        }
-        rec[i]=false;
-        return false;
-        
-    }
-    bool canFinish(int n, vector<vector<int>>& prerequisites) {
-        vector<bool> vis(n,false);
-        vector<bool> rec(n,false);
-        unordered_map<int,vector<int>> mpp=buildmap(prerequisites);
+    bool helper(int n,unordered_map<int,vector<int>>& mpp,vector<int>& indeg,vector<vector<int>> prereq){
+        int count=0;
+        queue<int> q;
         for(int i=0;i<n;i++){
-            if(!vis[i]&&isCycle(i,rec,vis,mpp))
-                return false;
+            if(indeg[i]==0){
+                count++;
+                q.push(i);
+            }
         }
-        return true;
+        while(!q.empty()){
+            int curr=q.front();
+            q.pop();
+            for(auto& it:mpp[curr]){
+                indeg[it]--;
+
+                if(indeg[it]==0){
+                    count++;
+                    q.push(it);
+                }
+            }
+
+        }
+        return count==n;
+    }
+    bool canFinish(int n, vector<vector<int>>& prereq) {
+        vector<int> indeg(n,0);
+        unordered_map<int,vector<int>> mpp=buildgraph(n,prereq,indeg);
+        return helper(n,mpp,indeg,prereq);
     }
 };
