3701c6a9ecdedd3f97fcaaa982982cf8a97ce611
Shivansh kumar <151950082+Shivansh3452@users.noreply.github.com>
2026-04-02 10:46:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -3,31 +3,37 @@ class Solution {
   
     vector<vector<int>> buildgraph(int V,vector<vector<int>> &edges){
         vector<vector<int>> adj(V);
-        for(auto& e: edges){
+        for(auto& e:edges)
             adj[e[0]].push_back(e[1]);
-        }
         return adj;
-    }
-    bool isCycle(vector<vector<int>>& adj,int u,vector<bool>& vis,vector<bool>& inRec){
-        vis[u]=true;
-        inRec[u]=true;
-        for(auto& v:adj[u]){
-            if(!vis[v]&&isCycle(adj,v,vis,inRec))
-                return true;
-            else if(inRec[v]==true)
-                return true;
-        }
-        inRec[u]=false;
-        return false;
     }
     bool isCyclic(int V, vector<vector<int>> &edges) {
         vector<vector<int>> adj=buildgraph(V,edges);
-        vector<bool>vis(V);
-        vector<bool>inRec(V);
-        for(int i=0;i<V;i++){
-            if(!vis[i]&&isCycle(adj,i,vis,inRec))
-                return true;
+        vector<int> indeg(V);
+        for(auto& u:adj){
+            for(auto& v:u)
+                indeg[v]++;
         }
-        return false;
+        int count=0;
+        queue<int> q;
+        for(int i=0;i<V;i++){
+            if(indeg[i]==0){
+                q.push(i);
+                count++;
+            }
+        }
+        while(!q.empty()){
+            int u=q.front();
+            q.pop();
+            for(auto& v:adj[u]){
+                indeg[v]--;
+                
+                if(indeg[v]==0){
+                    q.push(v);
+                    count++;
+                }
+            }
+        }
+        return !(count==V);
     }
 };
