a1585c32653135f6d3f0975dedd96a34e3ce1158
Shivansh kumar <151950082+Shivansh3452@users.noreply.github.com>
2026-04-03 15:03:40 +0530

    Time: 13 ms (13.34%) | Memory: 23.8 MB (5.06%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,41 +1,32 @@
 class Solution {
 public:
 
-    unordered_map<int,vector<int>> buildgraph(int n,vector<vector<int>> prereq,vector<int>& indeg){
-        unordered_map<int,vector<int>> mpp;
-        for(auto& e:prereq){
-            mpp[e[1]].push_back(e[0]);
-            indeg[e[0]]++;
-        }
-        return mpp;
-    }
-    bool helper(int n,unordered_map<int,vector<int>>& mpp,vector<int>& indeg,vector<vector<int>> prereq){
-        int count=0;
-        queue<int> q;
-        for(int i=0;i<n;i++){
-            if(indeg[i]==0){
-                count++;
-                q.push(i);
-            }
-        }
-        while(!q.empty()){
-            int curr=q.front();
-            q.pop();
-            for(auto& it:mpp[curr]){
-                indeg[it]--;
+    bool helper(int u,unordered_map<int,vector<int>>& mpp,vector<bool>& vis,vector<bool> inrec){
+        vis[u]=true;
+        inrec[u]=true;
 
-                if(indeg[it]==0){
-                    count++;
-                    q.push(it);
-                }
-            }
-
+        for(auto& v:mpp[u]){
+            if(!vis[v]&&helper(v,mpp,vis,inrec))
+                return true;
+            else if(inrec[v])
+                return true;
         }
-        return count==n;
+        inrec[u]=true;
+        return false;
     }
     bool canFinish(int n, vector<vector<int>>& prereq) {
-        vector<int> indeg(n,0);
-        unordered_map<int,vector<int>> mpp=buildgraph(n,prereq,indeg);
-        return helper(n,mpp,indeg,prereq);
+        vector<bool> vis(n,false);
+        vector<bool> inrec(n,false);
+
+        unordered_map<int,vector<int>> adj;
+        for(auto& e:prereq){
+            adj[e[0]].push_back(e[1]);
+        }
+        for(int i=0;i<n;i++){
+            if(!vis[i]&& helper(i,adj,vis,inrec)){
+                return false;
+            }
+        }
+        return true;
     }
 };
