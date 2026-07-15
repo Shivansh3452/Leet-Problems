49ccae1e2d3196a0d3235c2b65857085a267bd18
Shivansh kumar <151950082+Shivansh3452@users.noreply.github.com>
2026-07-15 23:10:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (85.02%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
+    int helper(int sumOdd,int sumEven){
+        return sumEven==0?sumOdd:gcd(sumEven,sumOdd%sumEven);
+    }
     int gcdOfOddEvenSums(int n) {
-        return n;
+        int sumOdd=n*n;
+        int sumEven=n*(n+1);
+        return helper(sumOdd,sumEven);
     }
 };
