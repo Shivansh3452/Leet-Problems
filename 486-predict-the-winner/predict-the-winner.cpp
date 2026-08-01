c963dcbcf2466f0cb158ba2d8f70d5d7ebe31d62
Shivansh kumar <151950082+Shivansh3452@users.noreply.github.com>
2026-08-01 23:16:39 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (69.71%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
-    int helper(int i,int n,vector<int>& nums){
-        if(i==n)
+    int t[100][100];
+    int helper(int i, int n, vector<int>& nums) {
+        if (i == n)
             return nums[i];
-            int frstone=nums[i]-helper(i+1,n,nums);
-            int lstone=nums[n]-helper(i,n-1,nums);
-        return max(frstone,lstone);
+        if (t[i][n] != -1)
+            return t[i][n];
+        int frstone = nums[i] - helper(i + 1, n, nums);
+        int lstone = nums[n] - helper(i, n - 1, nums);
+        t[i][n]= max(frstone, lstone);
+        return t[i][n];
     }
     bool predictTheWinner(vector<int>& nums) {
-        int n=nums.size();
-        int ans=helper(0,n-1,nums);
-        return ans>=0;
+        memset(t, -1, sizeof(t));
+        int n = nums.size();
+        int ans = helper(0, n - 1, nums);
+        return ans >= 0;
     }
 };
